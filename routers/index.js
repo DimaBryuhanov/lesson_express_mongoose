09eba6cfab4fd26f51c783c0506af392def6e0b6
User <user@Users-MacBook-Pro.local>
2020-08-23 09:20:01 +0300

    added: pug templates

--- routers/index.js
+++ routers/index.js
@@ -2,8 +2,9 @@ import express from 'express';
 
 var router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.send("Hi, there");
-});
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express with MongoDB' });
+  });
 
 export default router;
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 import express from 'express';
+import path from 'path';
+import pug from 'pug';
 import indexRouter from './routers/index.js';
 import config from './config/index.js';
 
 const app = express();
+const __dirname = path.resolve();
 
 // app.use(logger('dev'));
 // app.use(cookieParser());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 
