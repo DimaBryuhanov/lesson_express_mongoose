25d414547c72dc23cd120018b211ddaac902a524
User <user@Users-MacBook-Pro.local>
2020-08-23 08:44:52 +0300

    added: config file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-const PORT = 3000;
+import config from './config';
 
 const app = express();
 
@@ -7,6 +7,6 @@ app.get('/', (req, res, next) => {
     res.send("Hi, there");
 })
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`The server is listening on port ${PORT}`);
+app.listen(config.PORT, '0.0.0.0', () => {
+    console.log(`The server is listening on port ${config.PORT}`);
 });
