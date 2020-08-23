dfdab89abe22772b38df34844f516454d492258c
User <user@Users-MacBook-Pro.local>
2020-08-23 08:43:05 +0300

    added: simple express server functionality

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Hi');
+import express from 'express';
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.send("Hi, there");
+})
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`The server is listening on port ${PORT}`);
+});
