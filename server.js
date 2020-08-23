2a2bd13e458d88f97275ec420971d63df92f9349
User <user@Users-MacBook-Pro.local>
2020-08-23 09:25:41 +0300

    added: cookieParcer & logger

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 import express from 'express';
 import path from 'path';
 import pug from 'pug';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 import indexRouter from './routers/index.js';
 import config from './config/index.js';
 
 const app = express();
 const __dirname = path.resolve();
 
-// app.use(logger('dev'));
-// app.use(cookieParser());
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+// requests parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, 'public')));
+// Routs
 app.use('/', indexRouter);
+// addons
+app.use(logger('dev'));
+app.use(cookieParser());
 
 
 app.listen(config.PORT, '0.0.0.0', () => {
