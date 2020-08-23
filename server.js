f3aa9176fcca4dd1ce06c68d623808e478ffee5e
User <user@Users-MacBook-Pro.local>
2020-08-23 08:48:09 +0300

    added: app.use statements

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import config from './config';
 
 const app = express();
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res, next) => {
     res.send("Hi, there");
